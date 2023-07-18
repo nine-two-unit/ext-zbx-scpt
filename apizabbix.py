036aea7ff7e8791313e58cc0b094bd25c5638f17
Gabriel Silva <gabriel.guimaraes@via.com.br>
2023-07-18 15:27:10 -0300

    from mac

--- apizabbix.py
+++ apizabbix.py
@@ -1,6 +1,9 @@
 from pyzabbix import ZabbixAPI
 import configparser
 
+# Comentarios aleatorios de teste
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus diam id iaculis convallis. Sed non commodo eros. Suspendisse eu convallis enim. In posuere rutrum tristique. Morbi nec quam id est feugiat ultrices. Vestibulum pellentesque eu augue vel ultricies. Donec non nisi euismod, suscipit massa consectetur, gravida felis.
+
 def connect():
     config = configparser.ConfigParser()
     config.read("config.ini")
